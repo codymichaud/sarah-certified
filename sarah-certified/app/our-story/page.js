794d2fc499458e20c1b3298d953c0e9227813bd0
cody <cody@ascendcohealth.com>
2024-08-02 23:13:16 -0500

    our story content

--- sarah-certified/app/our-story/page.js
+++ sarah-certified/app/our-story/page.js
@@ -1,13 +1,29 @@
-
-
+import React from "react"
+import Image from 'next/image'
+import scspDark from '../../public/scsp-dark.png';
 
 export default function OurStory() {
 
     return (
         <main className=' min-h-screen dark text-foreground bg-background'>
-            <div className='flex flex-col items-center justify-center h-screen'>
-                <h1 className='text-4xl'>Our Story</h1>
-                <p className='text-lg'>We are a group of individuals who are passionate about helping others achieve their goals.</p>
+            <div className="flex flex-row">
+                <div className='flex flex-col w-1/2 items-center justify-center h-screen ml-20'>
+                    <h3 className='text-4xl'>Our Story</h3>
+                    <p className='text-lg w-11/12'>Our journey began with a frightening incident – a hospital visit due to an unlisted ingredient. 
+                    This experience, shared by me and my sisters, was a wake-up call. It led us to create Sarah Certified, a beacon of 
+                    safety in the culinary world. Our mission is simple: to ensure that every meal out is a pleasure, free from the 
+                    anxiety of allergen exposure.</p>
+                </div>
+                <div className="flex flex-col w-1/2 justify-center items-center">
+                    <Image
+                        alt="SCSP"
+                        src={scspDark}
+                        width={500}
+                        height={500}
+                        className='ml-16'
+                    />
+                    <h3>**Picture of Sarah and Lizzy?**</h3>
+                </div>
             </div>
         </main>
     )
