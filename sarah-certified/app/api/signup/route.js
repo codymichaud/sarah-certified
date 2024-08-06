dee8301dd275782f786c66a945da526d4949de65
cody <cody@ascendcohealth.com>
2024-08-06 09:35:38 -0500

    debugging database connection error

--- sarah-certified/app/api/signup/route.js
+++ sarah-certified/app/api/signup/route.js
@@ -4,43 +4,44 @@ import { NextResponse } from 'next/server';
 import { MongoClient } from 'mongodb';
 
 const client = new MongoClient(process.env.DIRECT_DATABASE_URL);
+let clientPromise;
+
+if (!clientPromise) {
+    clientPromise = client.connect();
+}
 
 export async function POST(request) {
-    console.log('request', request);
-
+    console.log('Request received:', new Date().toISOString());
     const { email, name, id } = await request.json();
 
     try {
-        console.log('connecting')
-        await client.connect();
+        console.log('Connecting to MongoDB:', new Date().toISOString());
+        await clientPromise;
+        console.log('Connected to MongoDB:', new Date().toISOString());
         const session = client.startSession();
 
         const transactionResults = await session.withTransaction(async () => {
             const db = client.db();
             const usersCollection = db.collection('User');
 
-            // Create a new user within the transaction
+            console.log('Inserting user into collection:', new Date().toISOString());
             const newUser = await usersCollection.insertOne({
                 email: email,
                 name: name,
                 id: id,
             }, { session });
-
+            console.log('User inserted:', new Date().toISOString());
             return newUser;
         });
 
+        console.log('Transaction results:', transactionResults, new Date().toISOString());
         if (transactionResults) {
-            console.log('Transaction successful:', transactionResults, transactionResults.ops);
             return NextResponse.json({ userID: transactionResults.insertedId, userName: name, email: email }, { status: 201 });
         } else {
-            console.log('Transaction failed');
             return NextResponse.json({ error: 'Transaction failed' }, { status: 500 });
         }
     } catch (err) {
-        console.error(err);
+        console.error('Error occurred:', err, new Date().toISOString());
         return NextResponse.json({ error: 'Failed to create user', details: err.message }, { status: 500 });
-    } finally {
-        await client.close();
-        await prisma.$disconnect();
     }
 }
